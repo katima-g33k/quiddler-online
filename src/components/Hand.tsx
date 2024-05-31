042b21e934f4cee3ff712184088462d4fab2a98d
Jessy Lachapelle <jessy.lachapelle@usherbrooke.ca>
2024-05-30 23:03:51 -0400

    Allow to move cards around in the hand

--- src/components/Hand.tsx
+++ src/components/Hand.tsx
@@ -1,13 +1,50 @@
 import React from "react";
 import HeldCard from "@/components/HeldCard";
 import { useGame } from "@/contexts/GameContext";
+import type { Card } from "@/types";
 
 const Hand = () => {
-  const { hand } = useGame();
+  const { hand, setHand } = useGame();
+
+  const handleMoveRight = (card: Card) => {
+    setHand((hand) => {
+      const index = hand.findIndex((c) => c.id === card.id);
+
+      return hand.map((c, i, cards) => {
+        if (i === index) {
+          return cards[i + 1];
+        }
+
+        if (i === index + 1) {
+          return card;
+        }
+
+        return c;
+      });
+    });
+  };
+
+  const handleMoveLeft = (card: Card) => {
+    setHand((hand) => {
+      const index = hand.findIndex((c) => c.id === card.id);
+
+      return hand.map((c, i, cards) => {
+        if (i === index) {
+          return cards[i - 1];
+        }
+
+        if (i === index - 1) {
+          return card;
+        }
+
+        return c;
+      });
+    });
+  };
 
   return (
     <div className="flex gap-2 flex-wrap">
-      {hand.map(card => <HeldCard key={card.id} card={card} />)}
+      {hand.map(card => <HeldCard key={card.id} card={card} onMoveLeft={handleMoveLeft} onMoveRight={handleMoveRight} />)}
     </div>
   );
 };
--- src/components/HeldCard.tsx
+++ src/components/HeldCard.tsx
@@ -1,15 +1,22 @@
-import React, { MouseEvent } from "react";
+import React, { MouseEvent, useState } from "react";
+import { ArrowLeft, ArrowRight } from "react-feather";
 import Card from "@/components/Card";
 import { useGame } from "@/contexts/GameContext";
 import type { Card as TCard } from "@/types";
 
 type HeldCardProps = {
   card: TCard;
+  onMoveLeft: (card: Card) => void;
+  onMoveRight: (card: Card) => void;
 }
 
-const HeldCard = ({ card }: HeldCardProps) => {
-  const { canPlay, selectedCards, setSelectedCards } = useGame();
+const HeldCard = ({ card, onMoveLeft, onMoveRight }: HeldCardProps) => {
+  const [hover, setHover] = useState(false);
+  const { canPlay, hand, selectedCards, setSelectedCards } = useGame();
   const selectedIndex = selectedCards.findIndex(c => c.id === card.id) + 1 || undefined;
+  const index = hand.findIndex((c) => c.id === card.id);
+  const isFirst = index === 0;
+  const isLast = index === hand.length - 1;
 
   const handleSelectCard = (event: MouseEvent<HTMLInputElement>) => {
     // @ts-ignore
@@ -23,13 +30,29 @@ const HeldCard = ({ card }: HeldCardProps) => {
   };
 
   return (
-    <Card
-      card={card}
-      disabled={!canPlay}
-      index={selectedIndex}
-      onClick={handleSelectCard}
-      type="checkbox"
-    />
+    <div
+      className="flex items-center relative"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {hover && !isFirst && (
+        <button className="absolute z-10 -left-1" onClick={() => onMoveLeft(card)}>
+          <ArrowLeft />
+        </button>
+      )}
+      <Card
+        card={card}
+        disabled={!canPlay}
+        index={selectedIndex}
+        onClick={handleSelectCard}
+        type="checkbox"
+      />
+      {hover && !isLast && (
+        <button className="absolute z-10 -right-1" onClick={() => onMoveRight(card)}>
+          <ArrowRight />
+        </button>
+      )}
+    </div>
   );
 };
 
