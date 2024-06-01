65755f5d40a2fac6c585803bbce36a7d8ffcf4d5
Jessy Lachapelle <jessy.lachapelle@usherbrooke.ca>
2024-06-01 17:17:30 -0400

    Prevent user from accidentaly closing the game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { GameProvider } from "@/contexts/GameContext";
 import Layout from "@/components/Layout";
 
 export default function App() {
+  useEffect(() => {
+    window.onbeforeunload = () => true;
+
+    return () => {
+      window.onbeforeunload = null;
+    };
+
+  }, []);
+
   return (
     <GameProvider>
       <Layout />
